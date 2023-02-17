35093aca881adae61c9ebdd363d37960ff435f68
Ardak <luxr2ge@gmail.com>
2023-02-17 23:31:22 +0600

    Initial Commit

---
+++ WeatherToday/WeatherTodayApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherTodayApp.swift
+//  WeatherToday
+//
+//  Created by Ardak Tursunbayev on 17.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherTodayApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
